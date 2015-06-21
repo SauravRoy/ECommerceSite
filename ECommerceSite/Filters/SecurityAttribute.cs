ecf748393efcc66e42a5b84ba49f54598c128794
Saurav <saurav.roy@accionlabs.com>
2015-06-21 06:26:40 -0700

    Added Custom Security Attribute

---
+++ ECommerceSite/Filters/SecurityAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace EcommerceServiceLayer.Filters
+{
+    public class CustomAuthorize : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if(filterContext.HttpContext.Request.Cookies.Count<=0)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                new RouteValueDictionary
+                {
+                    { "controller", "LogOn" },
+                    { "action", "Index" }
+                });
+            }
+        }
+        
+    }
+}
