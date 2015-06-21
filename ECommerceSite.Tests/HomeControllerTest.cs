c16c4f6c68450f163bc7334ba2fd24f1603f4fa3
Saurav <saurav.roy@accionlabs.com>
2015-06-21 07:16:33 -0700

    Unit test sample for HomeController

---
+++ ECommerceSite.Tests/HomeControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ECommerceSite.Controllers;
+using Moq;
+using CommonUtils;
+using System.Web.Mvc;
+
+namespace ECommerceSite.Tests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void TestDetailsView()
+        {
+
+            // Using MOQ for Mocking Dependencies .
+            var _logger = new Mock<ILogger>();
+            var _mail = new Mock<IMail>();
+
+            var controller = new HomeController(_logger.Object, _mail.Object);
+            var result = controller.Index() as ViewResult;
+            Assert.AreEqual("Index", result.ViewName);
+
+        }
+    }
+}
